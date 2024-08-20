e1d60084ea38896c3b7c8a06224ccbec6aaba546
Jocc22 <johnericblancia@gmail.com>
2024-08-20 11:34:17 +0800

    remove viewing middleware, and route in app

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const indexRouter = require("./routes");
-const viewingRouter = require("./routes/viewing");
 const adminRouter = require("./routes/admin");
 const downloadRouter = require("./routes/download");
 const {v4: uuidv4} = require("uuid");
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter); // Index Router that starts with forward slash('/') followed by /login, /getClassStudents, /getClassCodeDetails, /getCurrentSchoolYear, /getLoad
-app.use("/viewing", viewingRouter); //Viewing Router that starts with forward slash('/viewing') followed by /login, and /getGrades
 app.use("/admin", adminRouter); /* Viewing Router that starts with forward slash('/admin') followed by /getCurrentSchedule, /getEmails, /getSubjectLoad, /gradeSubmissionLogs, /downloadLogs, /updateClassCodeStatus, /updateSchedule */
 app.use("/download", downloadRouter); // Allowing download of logs
 app.get('/checkToken', async (req, res) => {
